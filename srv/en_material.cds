0196b832a62a028e548f8049f307e270ace8377e
Arquemiderr <Noobly54@gmail.com>
2024-04-08 23:59:27 +0000

    CRUD Terminado

--- srv/en_material.cds
+++ srv/en_material.cds
@@ -1,6 +1,9 @@
 using com.alfa as alfa from '../db/schema';
 
-service En_Material{
-    entity GetEnMaterial as projection on alfa.En_Material;
-    entity CreateMaterial as projection on alfa.En_Material;
+service En_Material {
+    // entity GetEnMaterial    as projection on alfa.En_Material;
+    // entity CreateMaterial   as projection on alfa.En_Material;
+    // entity UpdateEnMaterial as projection on alfa.En_Material;
+    // entity DeleteEnMaterial as projection on alfa.En_Material;
+    entity EnMaterial as projection on alfa.En_Material;
 }
--- srv/en_material.js
+++ srv/en_material.js
@@ -5,18 +5,18 @@ const { En_Material } = cds.entities("com.alfa");
 
 module.exports = (srv) => {
     //**********READ***********/
-    srv.on("READ", "GetEnMaterial", async (req) => {
+    srv.on("READ", "EnMaterial", async (req) => {
         return await SELECT.from(En_Material);
     });
 
     //**********CREATE***********/
-    srv.on("CREATE", "CreateMaterial", async (req) => {
+    srv.on("CREATE", "EnMaterial", async (req) => {
 
         let returnData = await cds
             .transaction(req)
             .run(
                 INSERT.into(En_Material).entries({
-                    folio_entrada: req.data.fecha_entrada,
+                    folio_entrada: req.data.folio_entrada,
                     id_proveedor: req.data.id_proveedor,
                     fecha_entrada: req.data.fecha_entrada,
                     no_articulos: req.data.no_articulos,
@@ -36,7 +36,52 @@ module.exports = (srv) => {
                 console.log(err);
                 req.error(err.code, err.message);
             });
-            console.log("Before End",returnData);
+        console.log("Before End", returnData);
         return returnData;
+    });
+
+    //**********UPDATE***********/
+    srv.on("UPDATE", "EnMaterial", async (req) => {
+        let returnData = await cds.transaction(req).run(
+            [
+                UPDATE(En_Material, req.data.folio_entrada).set({
+                    fecha_entrada: req.data.fecha_entrada,
+                    almacenista: req.data.almacenista
+                })
+            ]
+        ).then((resolve, reject) => {
+            console.log("Resolve: ", resolve);
+            console.log("Reject: ", reject);
+            if (resolve[0] == 0) {
+                req.error(409, "Record Not Found");
+            }
+        }).catch((err) => {
+            console.log(err);
+            req.error(err.code, err.message)
+        });
+        console.log("Before End", returnData);
+        return returnData;
+    });
+
+    //**********DELETE***********/
+    srv.on("DELETE", "EnMaterial",async (req) => {
+       let returnData =  await cds.transaction(req).run(
+            DELETE.from(En_Material).where({
+                folio_entrada: req.data.folio_entrada
+            })
+        ).then((resolve, reject) => {
+            console.log("Resolve: ", resolve);
+            console.log("Reject", reject);
+
+            if (resolve !== 1) {
+                req.error(409, "Record Not Found");
+            }
+        })
+            .catch((err)=>{
+                console.log(err);
+                req.error(err.code, err.message);
+            });
+            console.log("Before End", returnData);
+            return await returnData;
     });
 };
